56502e7e8282f4aa2482f68dba60bcdd54a7df17
Bálint Cserni <secret@secret.org>
2017-09-26 21:58:34 +0200

    Add homework.

---
+++ ant/src/vir/samples/HelloWorld.java
@@ -0,0 +1,13 @@
+package vir.samples;
+
+import org.apache.commons.logging.*;
+
+public class HelloWorld {
+
+	private static Log log = LogFactory.getLog(HelloWorld.class);
+
+	public static void main(String[] args) {
+        
+    	log.info("Hello World");
+    }
+}
---
+++ maven/src/main/java/vir/samples/App.java
@@ -0,0 +1,13 @@
+package vir.samples;
+
+import org.apache.commons.logging.*;
+
+public class App {
+
+	private static Log log = LogFactory.getLog(App.class);
+
+	public static void main(String[] args) {
+        
+    	log.info("Hello World");
+    }
+}
